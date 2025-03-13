3f17f7476553fbfec29954cb574a7eac3a257203
Himansha Kuruppuge <hkuruppuge@gmail.com>
2025-03-13 12:36:15 +0530

    Created and initialize server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
-console.log("hi")
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+
+
+let app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+let mongourl ="mongodb+srv://Admin:boola@cluster0.n2tnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(mongourl);
+
+let connection =mongoose.connection
+connection.once("open",()=>{
+    console.log("Mongodb successfully connected");
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server start");
+})
