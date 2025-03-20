2391b307074e806e7127a8606a0c2e9843c049d5
isukanimalasuriya <anujitha1234@gmail.com>
2025-03-20 09:37:07 +0530

    Index updated

--- backend/index.js
+++ backend/index.js
@@ -20,26 +20,6 @@ app.use(cors())
 
 app.use(bodyParser.json());
 
-app.use((req, res, next)=>{
-    let token = req.header
-    ("Authorization")
-
-    if(token!=null){
-        token = token.replace("Bearer ","")
-
-        jwt.verify(token, process.env.JWT_SECRET,
-            (err, decoded)=>{
-                if(!err){
-                    req.user = decoded
-                    //console.log(decoded)
-                }
-            }
-        )
-    }
-    next()
-    //console.log(token)
-})
-
 let mongourl = process.env.MONGO_URL
 
 mongoose.connect(mongourl);
