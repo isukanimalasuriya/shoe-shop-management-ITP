daf7714134d39b05676724f845629f7ce1ecf4f7
isukanimalasuriya <anujitha1234@gmail.com>
2025-03-20 11:36:48 +0530

    User model created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import productRouter from "../backend/routes/productRouter.js"
 import cartRoute from "./routes/cartRoute.js";
 import wishlistRoute from "./routes/wishlistRoute.js";
@@ -9,35 +9,21 @@ import customizeShoeRoute from "./routes/customizeshoueRoute.js";
 import userRouter from "./routes/userRouter.js"
 import dotenv from "dotenv"
 import cors from "cors"
-import router from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./DB/connectDB.js";
+import router from "./routes/authRouter.js";
 
-dotenv.config()
+dotenv.config();
 
 let app = express();
 
-app.use(cors())
-
-app.use(bodyParser.json());
-
-let mongourl = process.env.MONGO_URL
-
-mongoose.connect(mongourl);
-
-const connection =mongoose.connection
-connection.once("open",()=>{
-    console.log("Mongodb successfully connected");
+app.get("/", (req, res)=>{
+    res.send("Hello world")
 })
 
-app.use("/api/auth", router)
-app.use("/api/users", userRouter)
-app.use("/api/product",productRouter)
-app.use("/api/cart",cartRoute)
-app.use("/api/wishlist",wishlistRoute)
-app.use("/api/review",reviewRoute)
-app.use("/api/customize",customizeShoeRoute)
-
+app.use("/api/auth/", router)
 
 app.listen(3000,()=>{
-    console.log("Server starting on 3000 port");
+    connectDB()
+    console.log("Server starting on 3000 port")
 })
